a14e12d5cd79e26ded7ce9cd8a4ef3e22947742f
pavelp21 <pavelvp2182@yandex.ru>
2020-08-10 12:17:58 +0500

    Main page is typese

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,64 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend popup.js
 
-$(document).ready(function() {
+$(document).ready(function () {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
 
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+    $('.button-up').magnificPopup({
+        type: 'inline',
+        midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        mainClass: 'mfp-fade'
+    });
 
-
-/*Конец документа*/
+    /*Конец документа*/
 });
